af4ece900ceeb206fdcedd653f10acd50d519f3d
TwinB0rn <twinb0rn@github.com>
2021-10-31 17:25:04 -0500

    Updates

---
+++ precache-manifest.c769514e2e679ddfdaee4d89131bac23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d7c89fec15238f211aa34edf84fb6f",
+    "url": "/HIDIVE-HomePage/index.html"
+  },
+  {
+    "revision": "f9547473e651cff8764f",
+    "url": "/HIDIVE-HomePage/static/css/main.e7279ec6.chunk.css"
+  },
+  {
+    "revision": "c97b223aa493fdf20051",
+    "url": "/HIDIVE-HomePage/static/js/2.782646d5.chunk.js"
+  },
+  {
+    "revision": "c3887912b2e9cd301103d2e4a991e94d",
+    "url": "/HIDIVE-HomePage/static/js/2.782646d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9547473e651cff8764f",
+    "url": "/HIDIVE-HomePage/static/js/main.91f07dab.chunk.js"
+  },
+  {
+    "revision": "4fc166f8c5b1d5201113",
+    "url": "/HIDIVE-HomePage/static/js/runtime-main.38ac3431.js"
+  },
+  {
+    "revision": "85597ea9bc9d29c3c451cd11509c15d7",
+    "url": "/HIDIVE-HomePage/static/media/red-x.85597ea9.svg"
+  }
+]);
